edc44253d448a1b0022f7667076b3e8630daa02e
patrickkkim <patkim97@gmail.com>
2021-09-19 16:28:33 +0900

    Project 모델 관련 내용 추가

--- recruit/admin.py
+++ recruit/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Application, Estimate, Tag_Type, Tag, Project
 
 # Register your models here.
+admin.site.register(Application)
+admin.site.register(Estimate)
+admin.site.register(Tag_Type)
+admin.site.register(Tag)
+admin.site.register(Project)
--- recruit/models.py
+++ recruit/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here.
 class Application(models.Model):
     # project_id = models.ForeignKey(Project)
     # user_id = models.ForeignKey(User)
-    from_data_json = models.TextField()
+    form_data_json = models.TextField()
 
 
 class Estimate(models.Model):
@@ -19,5 +21,26 @@ class Tag_Type(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=10)
-    color = models.CharField(default="FFFFFF")
-    tag_type = models.ForeignKey(Tag_Type)
+    color = models.CharField(max_length=7, default="FFFFFF")
+    tag_type = models.ForeignKey(Tag_Type, on_delete=models.CASCADE)
+
+
+class Project(models.Model):
+    manager_id = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False, 
+        related_name="manager")
+    member_id = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, 
+        related_name="member")
+    estimate_id = models.ForeignKey(Estimate, on_delete=models.SET_NULL, blank=False, null=True)
+    tag_id = models.ForeignKey(Tag, on_delete=models.SET_NULL, blank=True, null=True)
+
+    title = models.CharField(max_length=100)
+    written_date = models.DateTimeField(auto_now=True, blank=False, null=False)
+    max_recruit = models.PositiveIntegerField(blank=False, null=False,
+        validators=[MinValueValidator(1), MaxValueValidator(100)])
+    description = models.TextField(max_length=2000, blank=False, null=False)
+    is_finished = models.BooleanField(blank=False, null=False, default=False)
+    question_list_json = models.TextField(max_length=2000, blank=True, null=True)
+
+    def __str__(self):
+        return self.title + " (팀장: {})".format(self.manager_id)
+
