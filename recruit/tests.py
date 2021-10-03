3e4be50bc924ce4f0a7130e80fb0e3741b3b60de
Dltmd202 <dltmd202@gmail.com>
2021-10-03 21:28:33 +0900

    draw up application method

--- recruit/tests.py
+++ recruit/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import *
+
 
 # Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_tag(self):
+        
